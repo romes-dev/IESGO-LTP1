9620015d7bc8b62d45c84a1f10620ad785f76f5b
Romes Heriberto <romes.heriberto@gmail.com>
2024-08-13 20:34:36 -0300

    ok

--- 2024/1 bimestre/Atividades/atividade_01.py
+++ 2024/1 bimestre/Atividades/atividade_01.py
@@ -1,1 +1,3 @@
 # Crie um programa em python em que o usuário insira o seu nome, seu número de matrícula e seu curso matriculado. Depois o programa deverá imprimir esses dados no terminal
+
+# dica: use as funções input() e print().
